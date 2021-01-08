5e5480c2f3fbfc8e2be09059245e014446c75ffc
Rayhan <rayhanjanam@gmail.com>
2021-01-08 17:13:14 +0600

    Add morgan

---
+++ src/middlewares/morgan.ts
@@ -0,0 +1,9 @@
+import { Application } from 'express';
+import morgan from 'morgan';
+
+class Morgan {
+  public static init(_express: Application): Application {
+    return _express.use(morgan('dev'));
+  }
+}
+export default Morgan;
